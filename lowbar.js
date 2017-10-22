a6624358218bd2c78da190785f0cb8091f41e9e3
Rosie Armstrong <rosemary_a@live.co.uk>
2017-10-22 21:46:35 +0100

    implement _.first

--- lowbar.js
+++ lowbar.js
@@ -4,4 +4,17 @@ _.identity = function (arg) {
     return arg;
 };
 
+_.first = function (arr, n = 1) {
+    if (!Array.isArray(arr) && !arr === 'string' || arr === null) return undefined;
+    if (n === 1) {
+        return arr[0];
+    } else {
+        if (typeof arr === 'string') {
+            return arr.slice(0, n).split('');
+        } else {
+            return arr.slice(0, n);
+        }
+    }
+};
+
 module.exports = _;
--- spec/lowbar.js
+++ spec/lowbar.js
@@ -23,3 +23,28 @@ describe('#identity', function () {
         expect(_.identity(NaN)).to.eql(NaN);
     });
 });
+
+describe('#first', function () {
+    it('is a function', function () {
+        expect(_.first).to.be.a('function');
+    });
+    it('returns undefined if anything other than an array or string is passed as an argument', function () {
+        expect(_.first({ name: 'foo' })).to.equal(undefined);
+        expect(_.first(10)).to.equal(undefined);
+        expect(_.first(NaN)).to.equal(undefined);
+        expect(_.first(null)).to.equal(undefined);
+        expect(_.first(0)).to.equal(undefined);
+    });
+    it('returns the first letter of a string if no number is passed as a second arguement', function () {
+        expect(_.first('string')).to.equal('s');
+    });
+    it('returns the first n elements of a string in an array when n is passed as a second arguement', function () {
+        expect(_.first('string', 3)).to.eql(['s', 't', 'r']);
+    });
+    it('returns the first element of an array if no number is given as a second argument', function () {
+        expect(_.first([1, 2, 3])).to.equal(1);
+    });
+    it('returns n elements from an array when passed n as a second argument', function () {
+        expect(_.first([1, 2, 3], 2)).to.eql([1, 2]);
+    });
+});
